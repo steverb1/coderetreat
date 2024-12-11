0967db6960e78c0930cd8d069b4e4f92377a34ec
steve.bement <steve.bement@accenture.com>
2024-12-11 10:21:10 -0700

    First bit of refactoring.

--- refactoring/movierental/java/src/main/java/movierental/Customer.java
+++ refactoring/movierental/java/src/main/java/movierental/Customer.java
@@ -25,30 +25,10 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each: _rentals) {
-            double thisAmount = 0;
-
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double thisAmount = calculateAmount(each);
 
             // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += getFrequentRenterPoints(each);
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -60,5 +40,34 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental each) {
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    private double calculateAmount(Rental rental) {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
--- refactoring/movierental/java/src/test/java/movierental/CustomerTest.java
+++ refactoring/movierental/java/src/test/java/movierental/CustomerTest.java
@@ -2,6 +2,7 @@ package movierental;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Tony on 1/15/2016.
@@ -11,5 +12,32 @@ public class CustomerTest {
     @Test
     public void sampleTest() throws Exception {
         Assert.assertTrue("First dummy test", true);
+    }
+
+    @Test
+    public void testStatement() {
+        Customer customer = new Customer("John Doe");
+        Movie movie1 = new Movie("Movie 1", Movie.REGULAR);
+        Movie movie2 = new Movie("Movie 2", Movie.NEW_RELEASE);
+        Movie movie3 = new Movie("Movie 3", Movie.CHILDRENS);
+
+        Rental rental1 = new Rental(movie1, 3); // applies rates for REGULAR movies
+        Rental rental2 = new Rental(movie2, 5); // applies rates for NEW_RELEASE movies
+        Rental rental3 = new Rental(movie3, 7); // applies rates for CHILDRENS movies
+
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+
+        String customerStatement = customer.statement();
+
+        String expectedStatement = "Rental Record for John Doe\n" +
+                "\tMovie 1\t3.5\n" +
+                "\tMovie 2\t15.0\n" +
+                "\tMovie 3\t7.5\n" +
+                "Amount owed is 26.0\n" +
+                "You earned 4 frequent renter points";
+
+        assertEquals(expectedStatement, customerStatement);
     }
 }
